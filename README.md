16fa31de93ea1da6bf4f66b61621b865a6d7e66e
Sivamani <106467109+5maurosivamani@users.noreply.github.com>
2024-08-07 19:33:35 +0530

    small animation with circle

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Output 1: Snow Man
 </h3>
 
 <img src="./git_src/snow-man.png" height="300">
+
+<h3>
+Output 2: Animation Circle
+</h3>
+<img src="./git_src/animation-1.png" height="300">
+<img src="./git_src/animation-2.png" height="300">
---
+++ main_2.js
@@ -0,0 +1,36 @@
+// Simple Animation through Canvas
+const canvas2 = document.getElementById("canvas-2");
+const context = canvas2.getContext("2d");
+
+const minX = 100,
+  rangeX = 200,
+  minRadios = 10,
+  rangeRadios = 30,
+  animationSpeed = 0.005;
+let p = 0,
+  sign = 1; // p stands for percentage (1 is 100%)
+
+animate();
+
+function animate() {
+  const x = minX + rangeX * p;
+  const radius = minRadios + rangeRadios * Math.sin(p * Math.PI);
+  p = p + animationSpeed * sign;
+
+  if (p > 1 || p < 0) {
+    // p = 0;
+    sign = sign * -1; // reverse the direction
+  }
+
+  context.clearRect(0, 0, canvas2.width, canvas2.height);
+  context.beginPath();
+  context.arc(x, 200, radius, 0, Math.PI * 2);
+  context.strokeStyle = "black";
+  context.stroke();
+  //   context.fillStyle = "black";
+  //   context.fill();
+
+  console.log(p);
+
+  requestAnimationFrame(animate); // call the animate function again after 16ms (60 fps)
+}
